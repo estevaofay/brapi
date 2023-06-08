93428764e9e2881a08151574bbe23242dbdbe885
Alisson Leal <alissonsleal@hotmail.com>
2023-06-08 15:17:36 -0300

    feat: add all available indexes

---
+++ utils/availableIndexes.tsx
@@ -0,0 +1,134 @@
+export const availableIndexes = [
+  {
+    stock: '^TWII',
+    name: 'TSEC weighted index',
+  },
+  {
+    stock: '^JN0U.JO',
+    name: 'Top 40 USD Net TRI Index',
+  },
+  {
+    stock: '^TA125.TA',
+    name: 'TA-125',
+  },
+  {
+    stock: '^STI',
+    name: 'STI Index',
+  },
+  {
+    stock: '^GSPTSE',
+    name: 'S&P/TSX Composite index',
+  },
+  {
+    stock: '^NZ50',
+    name: 'S&P/NZX 50 INDEX GROSS ( GROSS',
+  },
+  {
+    stock: '^IPSA',
+    name: 'S&P/CLX IPSA',
+  },
+  {
+    stock: '^AXJO',
+    name: 'S&P/ASX 200',
+  },
+  {
+    stock: '^BSESN',
+    name: 'S&P BSE SENSEX',
+  },
+  {
+    stock: '^GSPC',
+    name: 'S&P 500',
+  },
+  {
+    stock: '^RUT',
+    name: 'Russell 2000',
+  },
+  {
+    stock: '^NYA',
+    name: 'NYSE COMPOSITE (DJ)',
+  },
+  {
+    stock: '^XAX',
+    name: 'NYSE AMEX COMPOSITE INDEX',
+  },
+  {
+    stock: '^N225',
+    name: 'Nikkei 225',
+  },
+  {
+    stock: '^IXIC',
+    name: 'NASDAQ Composite',
+  },
+  {
+    stock: '^MERV',
+    name: 'MERVAL',
+  },
+  {
+    stock: '^KS11',
+    name: 'KOSPI Composite Index',
+  },
+  {
+    stock: '^MXX',
+    name: 'IPC MEXICO',
+  },
+  {
+    stock: '^JKSE',
+    name: 'IDX COMPOSITE',
+  },
+  {
+    stock: '^BVSP',
+    name: 'IBOVESPA',
+  },
+  {
+    stock: '^HSI',
+    name: 'HANG SENG INDEX',
+  },
+  {
+    stock: '^KLSE',
+    name: 'FTSE Bursa Malaysia KLCI',
+  },
+  {
+    stock: '^FTSE',
+    name: 'FTSE 100',
+  },
+  {
+    stock: '^N100',
+    name: 'Euronext 100 Index',
+  },
+  {
+    stock: '^STOXX50E',
+    name: 'ESTX 50 PR.EUR',
+  },
+  {
+    stock: '^CASE30',
+    name: 'EGX 30 Price Return Index',
+  },
+  {
+    stock: '^DJI',
+    name: 'Dow Jones Industrial Average',
+  },
+  {
+    stock: '^GDAXI',
+    name: 'DAX PERFORMANCE-INDEX',
+  },
+  {
+    stock: '^VIX',
+    name: 'CBOE Volatility Index',
+  },
+  {
+    stock: '^BUK100P',
+    name: 'Cboe UK 100',
+  },
+  {
+    stock: '^FCHI',
+    name: 'CAC 40',
+  },
+  {
+    stock: '^BFX',
+    name: 'BEL 20',
+  },
+  {
+    stock: '^AORD',
+    name: 'ALL ORDINARIES',
+  },
+];
