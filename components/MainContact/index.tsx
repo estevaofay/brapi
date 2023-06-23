b13080c53c0dc4aaf2894fb4c4b67339aefb1445
Alisson Leal <alissonsleal@hotmail.com>
2023-06-23 00:33:36 -0300

    feat: add linkedin company page to contact

--- components/MainContact/index.tsx
+++ components/MainContact/index.tsx
@@ -98,7 +98,7 @@ export const MainContact = () => {
             </form>
             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-800 flex flex-col items-center">
               <div className="w-fit flex flex-col">
-                <span className="leading-normal my-5 hover:text-white">
+                <span className="leading-normal hover:text-white">
                   <a
                     href="mailto:brapi@proton.me"
                     className="text-primary-400 flex space-x-2 items-center"
@@ -140,6 +140,33 @@ export const MainContact = () => {
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                     </svg>
                     <span>github</span>
+                  </a>
+                </span>
+
+                <span>
+                  <a
+                    href="https://www.linkedin.com/company/brapi.dev"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-white transition-colors flex space-x-2 items-center"
+                  >
+                    <svg
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                      fill="none"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                      <rect x="2" y="9" width="4" height="12"></rect>
+                      <circle cx="4" cy="4" r="2"></circle>
+                    </svg>
+                    <span>linkedin</span>
                   </a>
                 </span>
               </div>
