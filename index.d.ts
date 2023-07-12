be3ac759d97f95af8fd0943c727ec81723b1e3be
Alisson Leal <alissonsleal@hotmail.com>
2023-07-11 21:27:50 -0300

    feat: add mdx types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,13 @@
+declare module '*.mdx' {
+  export const meta: {
+    title: string;
+    author: string;
+    description?: string;
+    date?: string;
+    tags?: string[];
+  };
+}
+
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
