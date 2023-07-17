4d4785eeb510b0819696ecd7a4365c035400df37
Alisson Leal <alissonsleal@hotmail.com>
2023-07-17 11:12:59 -0300

    hotfix: use clarity-js instead of html script

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,20 +61,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
           `,
         }}
       />
-      <Script
-        id="clarity"
-        async
-        defer
-        dangerouslySetInnerHTML={{
-          __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "i08gpgdcs7");
-          `,
-        }}
-      />
 
       <body className="bg-gray-900">
         <Header />
