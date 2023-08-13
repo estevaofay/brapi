9d48ff092f09a0c54f1ce1580625dbb7836f3b68
Alisson Leal <alissonsleal@hotmail.com>
2023-08-13 03:53:24 -0300

    feat: create initial testing dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,21 @@
-export default function DashboardPage() {
+import { getServerSession } from 'next-auth';
+
+export default async function DashboardPage() {
+  const session = await getServerSession();
+
   return (
-    <div className="h-screen flex mt-12 justify-center text-4xl">Dashboard</div>
+    <div className="min-h-screen flex flex-col mt-12 container gap-4">
+      <h1 className="flex text-4xl font-bold items-center justify-center">
+        Dashboard
+      </h1>
+
+      <div className="items-center justify-center flex gap-2">
+        <img src={session.user.image} className="rounded-full w-16 h-16" />
+        <div className="flex flex-col">
+          <p className="text-2xl">{session.user.name}</p>
+          <p className="text-xl">{session.user.email}</p>
+        </div>
+      </div>
+    </div>
   );
 }
