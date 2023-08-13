65fa534e17b7a7ec66ef54e1903d1aed11a46c7a
Alisson Leal <alissonsleal@hotmail.com>
2023-08-13 19:07:50 -0300

    feat: add no token fallback

--- pages/api/token.tsx
+++ pages/api/token.tsx
@@ -5,6 +5,13 @@ import { createAPIToken } from '~/utils/apiToken';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const token = await getToken({ req });
 
+  if (!token) {
+    res.status(401).json({
+      error: 'Unauthorized',
+    });
+    return;
+  }
+
   const { token: apiToken } = createAPIToken({
     userId: token.userId,
   });
